76dd2106317176753a1ead855b0bdeb85225f4fb
clevercoolvip <panda18vishu@gmail.com>
2024-04-07 23:04:02 +0530

    Added new featuers

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from werkzeug.utils import secure_filename
 import os
 import cv2
 from placingNoBG_onTemplates import overlay_rgba_on_rgb
-from gpt_api import comp
+# from gpt_api import comp
 
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = "userUploadLOGO"
@@ -22,8 +22,9 @@ def logoUpload(file):
 def upload_file(templateS):
     if request.method == 'POST':
         file = request.files['file']
-        textarea = request.form.get("textarea")
-        print(textarea)
+        textarea_logo = request.form.get("textarea")
+        textarea_aboutus = request.form.get("textarea2")
+        textarea_contactus = request.form.get("textarea3")
         if file.filename == '':
             return redirect(request.url)
         if file:
@@ -32,7 +33,7 @@ def upload_file(templateS):
             file.save(upload_path)
             templatePath = "static/formatTemplates"
             MaxToken=300
-            result = comp(textarea, MaxToken, outputs=1)[0]
+            result = [textarea_logo, textarea_aboutus, textarea_contactus]
             
             outputFolder = os.path.join("static/generatedTemplates", "output.png")
             output_file_name.append("output.png")
@@ -49,53 +50,3 @@ def show():
 if __name__=="__main__":
     app.run(host='0.0.0.0', debug=True)
 
-"""
-from flask import Flask, render_template, request, url_for, redirect
-import os
-from placingNoBG_onTemplates import overlay_rgba_on_rgb
-from gpt_api import comp
-
-app = Flask(__name__)
-app.config["UPLOAD_FOLDER"] = "userUploadLOGO"
-output_file_name = []
-
-
-@app.route("/")
-def main():
-    imgs = os.listdir("static/formatTemplates")
-    return render_template("displayImage.html", images=imgs)
-
-@app.route("/logo/<string:file>")
-def logoUpload(file):
-    return render_template("fileUpload.html", img=f"{file}.PNG", endpoint=file)
-
-@app.route("/logo/<string:templateS>", methods=['GET', 'POST'])
-def upload_file(templateS):
-    if request.method == 'POST':
-        file = request.files['file']
-        textarea = request.form.get("textarea")
-        print(textarea)
-        filename = file.filename
-        template_name = f"{templateS}.PNG"
-        upload_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
-        file.save(upload_path)
-        templatePath = "static/formatTemplates"
-        MaxToken=300
-        result = comp(textarea, MaxToken, outputs=1)[0]
-        
-        outputFolder = os.path.join("static/generatedTemplates", "output.png")
-        output_file_name.append("output.png")
-        template = os.path.join(templatePath, template_name)
-        overlay_rgba_on_rgb(upload_path, template, outputFolder, x=5, y=5, text=result)
-
-        return render_template("generating_one_template.html", image="output.png", res=f"Generated string type has max length of {MaxToken}")
-
-@app.route("/show")
-def show():
-    return render_template("displayImage.html", images=output_file_name)
-
-
-
-if __name__=="__main__":
-    app.run(host='0.0.0.0', debug=True)
-"""
--- placingNoBG_onTemplates.py
+++ placingNoBG_onTemplates.py
@@ -2,17 +2,29 @@ import cv2
 from script5_fg_bg import get_bg
 from ocr_script import extract_text_with_coordinates
 import cv2
+import random
+import matplotlib.pyplot as plt
+
 
 def overlay_text_on_image(img, text, coordinates, font_scale, font_thickness, color):
     # Convert the coordinates to tuple format if not already in that format
+    print("In function TEXT_ON_IMAGE")
+    tt = random.choice(text)
     p1, p2 = coordinates[0], coordinates[2]
-    # Overlay text on the image
-    cv2.putText(img, text, tuple(p1), cv2.FONT_HERSHEY_SIMPLEX, font_scale, color, font_thickness)
 
-def overlay_text_from_ocr(image, text_with_coordinates, font_scale, font_thickness, color):
+    text_coord = [int((p1[0]+p2[0])/2), int((p1[1]+p2[1])/2)]
+
+    print(f"Image shape: {img.shape}")
+    img[p1[1]:p2[1], p1[0]:p2[0]] = 255
+    # Overlay text on the image
+    print("Coordinate parameter: ", coordinates)
+    print(f"p1: {p1} | p2: {p2}")
+    cv2.putText(img, tt, tuple(text_coord), cv2.FONT_HERSHEY_SIMPLEX, font_scale, color, font_thickness)
+
+def overlay_text_from_ocr(image, text_with_coordinates, font_scale, font_thickness, color, text):
     for title, title_coordinates, context in text_with_coordinates:
         # Assuming title_coordinates is a list of coordinates, we'll take the first one
-        overlay_text_on_image(image, "Honda Motors", title_coordinates, font_scale, font_thickness, color)
+        overlay_text_on_image(image, text, title_coordinates, font_scale, font_thickness, color)
 
 ##def overlay_text_on_image(img, text, coordinates, font_scale, font_thickness, color):
 ##    cv2.putText(img, text, coordinates, cv2.FONT_HERSHEY_SIMPLEX, font_scale, color, font_thickness)
@@ -30,82 +42,10 @@ def overlay_text_from_chatgpt(image, generated_text, coordinates, font_scale, fo
         new_coordinates = (coordinates[0], coordinates[1] + i * font_scale * 20)
         overlay_text_on_image(image, line, new_coordinates, font_scale, font_thickness, color)
 
-# def overlay_rgba_on_rgb(logoFilePath, rgb_template_path, outputFolder, x=0, y=0, chatgpt_text="ChatGPT generated text"):
-#     rgba_overlay = get_bg(logoFilePath)
-#     rgb_image = cv2.imread(rgb_template_path)
-
-#     text_with_coordinates = extract_text_with_coordinates(rgb_image)
-
-#     overlay_text_from_ocr(rgb_image, text_with_coordinates, font_scale=0.5, font_thickness=2, color=(0, 0, 0))
-
-#     # overlay_text_from_chatgpt(rgb_image, chatgpt_text, coordinates=(x, y), font_scale=0.5, font_thickness=2, color=(255, 0, 0))
-
-#     cv2.imwrite(outputFolder, rgb_image)
-#     return rgb_image
-
-
-
-
-
-# def put_text_with_word_wrap(img, text, container_pos, container_size, font_scale, font_thickness, color):
-#     # Create a copy of the image to draw on
-#     img_with_text = img.copy()
-
-#     # Define the position and size of the container
-#     container_x, container_y = container_pos
-#     container_width, container_height = container_size
-
-#     # Define the padding for the text
-#     padding_x = 10
-#     padding_y = 10
-
-#     # Split the text into words
-#     words = text.split()
-
-#     # Initialize variables to track the current line and position
-#     current_line = ''
-#     current_width = 0
-#     current_height = 0
-
-#     # Loop through each word in the text
-#     for word in words:
-#         # Get the size of the current line with the new word added
-#         size = cv2.getTextSize(current_line + word, cv2.FONT_HERSHEY_SIMPLEX, font_scale, font_thickness)
-
-#         # If adding the new word would make the line too wide, start a new line
-#         if current_width + size[0][0] + padding_x > container_width:
-#             current_line = ''
-#             current_height += size[0][1] + padding_y
-#             current_width = 0
-
-#         # Add the word to the current line
-#         current_line += word + ' '
-#         current_width += size[0][0] + padding_x
-
-#         # If the current line exceeds the height of the container, stop adding words
-#         if current_height + size[0][1] > container_height:
-#             break
-
-#         # Draw the current line on the image
-#         cv2.putText(img_with_text, current_line, (container_x + padding_x, container_y + current_height + size[0][1]), cv2.FONT_HERSHEY_SIMPLEX, font_scale, color, font_thickness)
-
-#     return img_with_text
-
-
 
 def overlay_rgba_on_rgb(logoFilePath, rgb_template_path, outputFolder, x=0, y=0, text="OpenCV contains putText() method which is used to put text on any image. The method uses following parameters. img: The Image on which you want to write the text. text: The text you want to write on the image"):
     rgba_overlay = get_bg(logoFilePath)
     rgb_image = cv2.imread(rgb_template_path)
-
-    text_with_coordinates = extract_text_with_coordinates(rgb_image)
-
-    overlay_text_from_ocr(rgb_image, text_with_coordinates, font_scale=0.5, font_thickness=2, color=(0, 0, 0))
-
-    # overlay_text_from_chatgpt(rgb_image, chatgpt_text, coordinates=(x, y), font_scale=0.5, font_thickness=2, color=(255, 0, 0))
-
-    cv2.imwrite(outputFolder, rgb_image)
-
-
 
     # rgba_overlay = cv2.imread(rgba_overlay_path, -1)
     overlay_alpha = rgba_overlay[:, :, 3] / 255.0  # Normalize alpha channel (0.0 - 1.0)
@@ -118,21 +58,11 @@ def overlay_rgba_on_rgb(logoFilePath, rgb_template_path, outputFolder, x=0, y=0,
 
     # Convert back to RGB if desired
     rgb_image_with_overlay = cv2.cvtColor(bgra_image, cv2.COLOR_RGBA2RGB)
-
-
-
-
-    container_pos = (100, 100)
-    container_size = (300, 200)
-    font_scale=0.3
-    font_thickness = 1
-    color = (0, 0, 255)
-
     # rgb_image_with_overlay = put_text_with_word_wrap(rgb_image_with_overlay, text, container_pos, container_size, font_scale, font_thickness, color)
     
     text_with_coordinates = extract_text_with_coordinates(rgb_image_with_overlay)
 
-    overlay_text_from_ocr(rgb_image_with_overlay, text_with_coordinates, font_scale=0.5, font_thickness=2, color=(0, 0, 0))
+    overlay_text_from_ocr(rgb_image_with_overlay, text_with_coordinates, text=text, font_scale=1, font_thickness=2, color=(0, 0, 255))
 
     # overlay_text_from_chatgpt(rgb_image, chatgpt_text, coordinates=(x, y), font_scale=0.5, font_thickness=2, color=(255, 0, 0))
 
@@ -144,7 +74,7 @@ def overlay_rgba_on_rgb(logoFilePath, rgb_template_path, outputFolder, x=0, y=0,
 
 
 if __name__=="__main__":
-    result_image = overlay_rgba_on_rgb("static/formatTemplates/1.PNG", "noBGOutput/test.png", "static/generatedTemplates/output__1.png", x=5, y=5)
+    result_image = overlay_rgba_on_rgb("static/formatTemplates/2.PNG", "noBGOutput/test.png", "static/generatedTemplates/output__1.png", x=5, y=5)
 
     # Display or save the result
     cv2.imshow("Image with Overlay", result_image)
